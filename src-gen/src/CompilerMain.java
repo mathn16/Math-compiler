522a05a62dfcca568a8381c99c0d681278fa493e
mathn16 <Mathias626@hotmail.com>
2020-04-27 19:32:07 +0200

    Uploaded assigment
    contains:
     - xtext
     - xtend generator
     - Generated java artifacts

---
+++ src-gen/src/CompilerMain.java
@@ -0,0 +1,8 @@
+public class CompilerMain {
+
+    public static void main(String args[]){
+        MathComputation mc = new MathComputation();
+        mc.compute();
+    }
+
+}
---
+++ src-gen/src/MathComputation.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class MathComputation {
+
+	public MathComputation() {
+
+	}
+
+	public void compute() {
+		System.out.println("first value is: " + (1+2));
+		System.out.println("second value is: " + (3));
+		System.out.println("third value is: " + (4+2*3));
+	}
+}
